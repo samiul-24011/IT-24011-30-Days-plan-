78bb2c0b3011c1ae9ffafbf17316ff55b80dfbe0
samiul-24011 <it24011@mbstu.ac.bd>
2026-04-22 09:37:05 +0600

    Create Main.java

---
+++ Day-1(19-04-2026)/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="Black";
+  myCar.model="BMW";
+  myCar.year=2026;
+  myCar.displayInfo();
+    }
+}
